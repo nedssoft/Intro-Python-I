a1038c351f85b66ab14d451900bec929561364ad
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-10 17:06:34 +0100

    Manipulate dictionaries

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,12 +36,25 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+            
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
+waypoints.append({'lat': 54, 'lon': -124, 'name': 'a fourth place'})
+for (i,d) in enumerate(waypoints):
+    for k,v in d.items():
+        if v=='a place':
+            waypoints[i]['lon'] = -130
+            waypoints[i]['name'] = 'real place'
+
+print(waypoints)
 # YOUR CODE HERE
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for (i,d) in enumerate(waypoints):
+    for k,v in d.items():
+        print(f'Item {i} field {k} is {v}')
